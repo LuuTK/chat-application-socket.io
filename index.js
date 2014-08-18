4a74131aa0815180f4f3e7040fe755aa21e8fe9d
TiKaYLoU <tuan-kiet.luu@mail.mcgill.ca>
2014-08-18 09:57:38 -0400

    initial

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
-
+ 
 http.listen(3000, 
 	function(){
 	console.log('listening on *:3000');
